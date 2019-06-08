bd0d0e7f2d3730fa57fbee732e0e3a8bf5ce31c1
Tommy Parnell <tommy@terribledev.io>
2019-06-07 23:22:11 -0400

    Updates

---
+++ precache-manifest.7210e3ba5d4bcf982c659bafbc6a537c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9eb54c09da9281355e83f1bc6f3c1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6bda298cea79b148775",
+    "url": "/static/css/main.66e69401.chunk.css"
+  },
+  {
+    "revision": "64f99f37926f242efd6c",
+    "url": "/static/js/2.7362c33c.chunk.js"
+  },
+  {
+    "revision": "e6bda298cea79b148775",
+    "url": "/static/js/main.62367d74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
